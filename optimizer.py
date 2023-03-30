55c70fd701cc9e4fc500bf8c668c1c59253db129
alaae <aalaeddine.maggouri@emines.um6p.ma>
2023-03-30 13:19:10 +0000

    not verified yet

--- optimizer.py
+++ optimizer.py
@@ -9,22 +9,29 @@ T_Ci, assets,slr,region = pg.generator()
 prob = LpProblem('assets_protection', LpMinimize)
 
 # decision variables+ objective function
-x = []
+x = np.zeros(region.shape()[0], range(8))
 obj = []
 for i in region.shape()[0]:
     for j in range(8):
-        x.append(LpVariable(name="x{i,j}", cat='Binary')) #
-        obj.append(x[-1]*(slr-region[i]))
+        x[i,j] = LpVariable(name=f"x{i,j}", cat='Binary') #
+        obj.append(x[i,j]*(slr-region[i]))
 # objective function
 prob += lpSum(obj)
-
+x = np.array(x, ())
 for asset_label in T_Ci.keys():
     for road_labels in T_Ci[asset_label]:
         road_labels = [road_labels[0]]+road_labels # convention: x_ii =1 for i in entry means that we should place an barrier on the sea-entry i border
         temp = []
-        for zone_label in road_labels:
-            temp.append(x[])
+        for i in range(len(road_labels)-1):
+            temp.append(x[road_labels[i+1],road_labels[i]])
+        prob += lpSum(temp)
 
+status = prob.solve()
 
-
+# Print the values of the decision variables
+for i in region.shape()[0]:
+    for j in range(8):
+        if x[i,j].value() ==1 : print(f"x{i,j} = {x[i,j].value()}")
+        obj_value = value(prob.objective)
+print(f"Objective function value: {obj_value}")
 
